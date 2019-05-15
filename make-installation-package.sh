c03af5b27a854467e2d504d49925a808faef8674
Tommi Palomäki <tommi.palomaki@digia.com>
2019-05-15 06:28:38 +0000

    AIX tar does not support strip-components flag => the package must be created differently

--- make-installation-package.sh
+++ make-installation-package.sh
@@ -2,6 +2,6 @@
 set -e
 
 # This script makes a tar.gz package for scripts installation under AIX
-pushd etc
-tar zcvf ../zabbix-monitoring-scripts-aix.tar.gz zabbix/
+pushd etc/zabbix/
+tar zcvf ../../zabbix-monitoring-scripts-aix.tar.gz *
 popd
