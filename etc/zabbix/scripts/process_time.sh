52aa2a88b45654c421f826fa16e31c744cfdd85a
Mika Järvenpää <mika.jarvenpaa@digia.com>
2019-05-17 11:32:11 +0000

    Update comments

--- etc/zabbix/scripts/process_time.sh
+++ etc/zabbix/scripts/process_time.sh
@@ -1,13 +1,21 @@
 #/opt/freeware/bin/bash
 set -e
-# @args process's name
-# @input eg. '00:00:17'
-#            '00:00:13'
-# @output 30
+# @args process_name
+#
+# exmaple data:
+# zabbix_agentd 00:00:17
+# zabbix_agentd 00:00:13
+#
+# @output elapsed CPU time of the given process, summing up all CPU times of processes with identical name
+# example output:
+# 30
+
 # 1. 'ps -A -o comm= -o time= ' prints all process columns
 # 2. 'grep "^$1 "' grep rows where wanted process name
 # 3. 'sed 's/^.*\([0-9:]\{8\}\)$/\1/g'' sed everything else than numbers off
 # 4. 'sed 's/:/ /g'' sed ':' characters out from time and replace with whitespace
 # 5. 'awk '{print $3 + $2 * 60 + $1 * 3600}'' sum seconds minutes and hours together
+# 6. 'xargs' for converting standard input into arguments
+# 7. 'sed 's/ /+/g'' replaces whitespaces with '+'. So the results is like mathematic formula.
 
 ps -A -o comm= -o time= | grep "^$1 " | sed 's/^.*\([0-9:]\{8\}\)$/\1/g' | sed 's/:/ /g' | awk '{print $3 + $2 * 60 + $1 * 3600}' | xargs | sed 's/ /+/g' | bc
