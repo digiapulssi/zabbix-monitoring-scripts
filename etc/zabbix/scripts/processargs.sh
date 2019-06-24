3c18c7044ddef4765b8b4f0145489f637c3942b1
Mika Järvenpää <mika.jarvenpaa@digia.com>
2019-06-24 15:44:30 +0300

    Support for commandline arguments

--- etc/zabbix/scripts/processargs.sh
+++ etc/zabbix/scripts/processargs.sh
@@ -1,28 +1,27 @@
 #!/opt/freeware/bin/bash
-# Version: 0.1
+# Version: 0.5
 set -e
 
-# Discover all running process with same process name (first argument)
-# Use the process name in /proc/pid/status that's truncated to 15 characters
-# because it's the most reliable name used by Zabbix process monitoring items
-# See https://www.zabbix.com/documentation/3.0/manual/appendix/items/proc_mem_num_notes
-
+# Discover all running process with same process name (first cmdline parameter)
+# Prints process's firt startup argument (second cmdline parameter)
+# Prints process's second startup argument (third cmdline parameter)
 echo -n '{"data":['
 # Uses first command line argument to show chosen process with startup arguments
 # Filter away processes with no cumulative CPU time with grep -v
 # Filter away kernel processes (and also zombie processes) by filtering out processes that don't use any user memory (vsz == 0)
-# Captures first, fifth and seventh string (defaults for process name, IIBNODE name and excecuiton group)
-# -> Script is currently dependet of the amount of start up parameters
-# Output: json
-# {
-#	  "data": [
-#		  {
-#			  "{#COMMAND}": "DataFlowEngine",
-#			  "{#IIBNODE}": "IBEAISANDBOX",
-#		  	"{#EXCECUTIONGROUP}": "SERVICEGRP1"
-#	    }
-#	  ]
-# }
+# Captures first, $2:th and $3:th string
+# Usage:
+# >> ./processargs.sh DataFlowEngine 5 7
+# Output:
+# << {
+#	<<  "data": [
+#	<<	  {
+#	<<		  "{#COMMAND}": "DataFlowEngine",
+#	<<		  "{#IIBNODE}": "IBEAISANDBOX",
+#	<<	  	"{#EXCECUTIONGROUP}": "SERVICEGRP1"
+#	<<    }
+#	<<  ]
+# << }
 
-ps -A -o comm= -o time= -o vsz= -o args= | grep "$1" | grep -v ' 00:00:00' | awk '$3 != 0' | awk '{print $1 " " $5 " " $7}' | sed 's/\(.*\) \(.*\) \(.*\)/{"{#COMMAND}":"\1", "{#IIBNODE}":"\2", "{#EXCECUTIONGROUP}":"\3"}/g' | sed '$!s/$/,/' | tr '\n' ' '
-echo -n ']}'
+ps -A -o comm= -o time= -o vsz= -o args= | grep "$1" | grep -v ' 00:00:00' | awk '$3 != 0' | awk -v a="$2" -v b="$3" '{print $1 " " $a " " $b}' | sed 's/\(.*\) \(.*\) \(.*\)/{"{#COMMAND}":"\1", "{#IIBNODE}":"\2"}, "{#EXCECUTIONGROUP}":"\
+3"}/g' | sed '$!s/$/,/' | tr '\n' ' '
