55b4c2b7862b46a3c562e3c2031fd4d5d2637ed4
Mika Järvenpää <mika.jarvenpaa@digia.com>
2019-06-25 09:49:18 +0300

    Update comments

--- etc/zabbix/scripts/process_time.sh
+++ etc/zabbix/scripts/process_time.sh
@@ -1,7 +1,7 @@
 #/opt/freeware/bin/bash
 # Version 1.3
 set -e
-# @args process_name and startup_parameter (optional)
+# @args process_name, first_startup_parameter_filter (optional) and second_startup_parameter_filter (optional)
 #
 # exmaple data:
 # zabbix_agentd 00:00:17
@@ -10,10 +10,10 @@ set -e
 # @output elapsed CPU time of the given process, summing up all CPU times of processes with identical name
 # example output:
 # 30
-# If process is not found from running processes, returned value is 0
 
 # 1. 'ps -A -o comm= -o time= ' prints all process columns
 # 2. 'grep "^$1 "' grep rows where wanted process name
+# 2.1 'grep "$2"' and 'grep "$3"' used as filter for specific sub-process
 # 3. 'sed 's/^.*\([0-9:]\{8\}\)$/\1/g'' sed everything else than numbers off
 # 4. 'sed 's/:/ /g'' sed ':' characters out from time and replace with whitespace
 # 5. 'awk '{print $3 + $2 * 60 + $1 * 3600}'' sum seconds minutes and hours together
@@ -33,6 +33,7 @@ else
   echo "Illegal number of arguments. 1st argument = process_name 2nd argument (optional) = process's startup argument"
 fi
 
+# If process is not found from running processes, returned value is 0
 if [ -z "${val}" ] ; then
         echo "0"
 else
