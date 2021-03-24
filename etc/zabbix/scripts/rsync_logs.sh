fbfc6c5e7aa6d87a940b1bbe61a02b9e351439a5
ansalapo <53914958+ansalapo@users.noreply.github.com>
2021-03-24 07:43:27 +0200

    Update rsync_logs.sh

--- etc/zabbix/scripts/rsync_logs.sh
+++ etc/zabbix/scripts/rsync_logs.sh
@@ -2,15 +2,17 @@
 
 # Transfer AIX application logs to proxy server via rsync
 
-SOURCEFILE=$1
-DESTINATIONPATH=$2
-DESTINATIONSERVER=$3
-RSYNCVARS=$4
+set -e
+
+SOURCEFILE="$1"
+DESTINATIONPATH="$2"
+DESTINATIONSERVER="$3"
+RSYNCVARS="$4"
 
 # Check that rsync variables file exists and create one if not
 if [ ! -f "$RSYNCVARS" ]; then
   touch "$RSYNCVARS"
-  cat > $RSYNCVARS << EOL
+  cat > "$RSYNCVARS" << EOL
   PREVSIZE=0
   INODE=0
 EOL
@@ -28,19 +30,23 @@ LOGFILESIZE=$(wc -c "$SOURCEFILE" | awk '{print $1}')
 if [ $NEWINODE -eq $INODE ]; then
 
   if [ $LOGFILESIZE -gt $PREVSIZE ]; then
-    rsync -za --append -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $SOURCEFILE rsync@$DESTINATIONSERVER:$DESTINATIONPATH
+    rsync -za --append -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" "$SOURCEFILE" rsync@$DESTINATIONSERVER:"$DESTINATIONPATH"
   else
-    rsync -za -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $SOURCEFILE rsync@$DESTINATIONSERVER:$DESTINATIONPATH
+    rsync -za -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" "$SOURCEFILE" rsync@$DESTINATIONSERVER:"$DESTINATIONPATH"
   fi
 else
   # If the logfile has just rotated, find the rotated version and transfer remaining logs
-  OLDLOGFILEPATH=$(dirname "$SOURCEFILE")
-  OLDLOGFILEPATH=$(find "$OLDLOGFILEPATH" -inum "$INODE")
-  rsync -za --append -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" $OLDLOGFILEPATH rsync@$DESTINATIONSERVER:$DESTINATIONPATH
+  if [ ! $INODE -eq "0" ]; then
+    OLDLOGFILEPATH=$(dirname "$SOURCEFILE")
+    OLDLOGFILEPATH=$(find "$OLDLOGFILEPATH" -inum "$INODE" 2>/dev/null || true)
+    if [ ! -z "$OLDLOGFILEPATH" ]; then
+      rsync -za --append -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" "$OLDLOGFILEPATH" rsync@$DESTINATIONSERVER:"$DESTINATIONPATH"
+    fi
+  fi
 fi
 
 # Set variables in helper file
-cat > $RSYNCVARS << EOL
+cat > "$RSYNCVARS" << EOL
 PREVSIZE=${LOGFILESIZE}
 INODE=${NEWINODE}
 EOL
